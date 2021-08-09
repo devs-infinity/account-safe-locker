8290bd0b4382debd03cef4f1ff44296a1e1dc968
Josh Daniel <joshpersonal8@gmail.com>
2021-08-09 11:09:24 +0800

    connect to mongoDB via mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,6 +13,13 @@ app.use(cors());
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+mongoose.connection.on("connected", () => console.log("Database is connected!"));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
