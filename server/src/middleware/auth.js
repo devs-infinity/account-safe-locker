8112a04391c7553cf915814b3aeffbcdaeb002bc
bryan <bryanavila998@gmail.com>
2021-08-09 22:49:08 +0800

    addded auth middleware

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/User'
+
+export const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'testkey')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        
+        if (!user) {
+            throw new Error()
+        }
+
+        req.token = token
+        req.user = user
+
+        next()
+    } catch (e) {
+        res.status(400).send({
+            errorMessage: 'Please Authenticate'
+        })
+    }
+}
