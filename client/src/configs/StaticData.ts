82a970b3071c961dff2d0762cc67544bab1909af
Zomer <zomerzxc.19@gmail.com>
2021-08-12 00:01:27 +0800

    Add navlinks data and types

---
+++ client/src/configs/StaticData.ts
@@ -0,0 +1,12 @@
+import { INavLinks } from '../typings/StaticDataTypes';
+
+export const navLinks: INavLinks[] = [
+  {
+    name: 'About us',
+    url: '#about',
+  },
+  {
+    name: 'Contact',
+    url: '#contact',
+  },
+];
