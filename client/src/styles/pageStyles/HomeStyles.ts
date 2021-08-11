38e9cfdb2336a768c016fc96fdf9b1ad95b6a027
Zomer <zomerzxc.19@gmail.com>
2021-08-12 02:56:54 +0800

    Start styling home page

--- client/src/styles/pageStyles/HomeStyles.ts
+++ client/src/styles/pageStyles/HomeStyles.ts
@@ -6,12 +6,67 @@ export const StyledHome = styled.main`
 `;
 
 export const StyledFormSection = styled.section`
-  height: calc(100vh - var(--nav-height));
+  height: 600px;
+  width: 1200px;
   display: flex;
   justify-content: center;
+  margin: 0 auto;
 
-  .brand-name {
+  .branding {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 60%;
     background-color: var(--navy-blue);
+    position: relative;
+  }
+
+  .brand-items {
+    color: var(--white);
+  }
+
+  .lock-icon {
+    height: 50px;
+    width: 50px;
+    margin-right: 10px;
+  }
+
+  .brand-name {
+    font-size: 48px;
+    font-weight: 800;
+  }
+
+  .decor-1 {
+    top: 20px;
+    left: 110px;
+  }
+
+  .decor-2 {
+    right: -20px;
+    bottom: 130px;
+  }
+
+  .decor {
+    position: absolute;
+
+    ::before {
+      content: '';
+      position: absolute;
+      height: 45px;
+      width: 200px;
+      top: 0;
+      right: 0;
+      background-color: var(--blue);
+    }
+
+    ::after {
+      content: '';
+      position: absolute;
+      right: 0;
+      bottom: -110px;
+      height: 45px;
+      width: 150px;
+      background-color: var(--blue);
+    }
   }
 `;
