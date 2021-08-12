cf4d38be3cc23011d5039734ed56dd4cac2b9cfe
Josh Daniel <joshpersonal8@gmail.com>
2021-08-12 15:11:52 +0800

    add styles to signup form

--- client/src/components/HomeForm.tsx
+++ client/src/components/HomeForm.tsx
@@ -4,7 +4,19 @@ import { StyledHomeForm } from '../styles/componentStyles/HomeFormStyles';
 interface IHomeFormProps {}
 
 const HomeForm: React.FC<IHomeFormProps> = ({}) => {
-  return <StyledHomeForm>SIGN IN/UP FORM</StyledHomeForm>;
+
+  return (
+    <StyledHomeForm>
+      <form>
+        <h1>SIGN UP</h1>
+        <p>Never forget for a better Tomorrow</p>
+        <input type='text' placeholder='Username' />
+        <input type='email' placeholder='Email' />
+        <input type='password' placeholder='Choose your password' />
+        <button type='submit'>Sign Up</button>
+      </form>
+    </StyledHomeForm>
+  );
 };
 
 export default HomeForm;
--- client/src/styles/componentStyles/HomeFormStyles.ts
+++ client/src/styles/componentStyles/HomeFormStyles.ts
@@ -2,5 +2,41 @@ import styled from 'styled-components';
 
 export const StyledHomeForm = styled.div`
   width: 40%;
+  padding: 1rem;
   background-color: var(--light-gray);
+
+  form {
+    display: flex;
+    margin: 0 auto;
+    height: 100%;
+    width: 90%;
+    flex-direction: column;
+    text-align: left;
+
+    h1 {
+      font-weight: 800;
+      margin-top: 1rem;
+    }
+
+    p {
+      font-weight: 500;
+      margin: 1.2rem 0 2rem 0;
+    }
+
+    input {
+      outline: none;
+      border: none;
+      font-family: inherit;
+      padding: 0.5rem;
+      font-size: 1rem;
+      margin-bottom: 1.5rem;
+      border-radius: 0.3rem;
+    }
+
+    button {
+      background: var(--navy-blue);
+      color: var(--white);
+      padding: 0.6rem;
+    }
+  }
 `;
