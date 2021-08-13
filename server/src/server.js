6e789ab4217b9f8b55e6e2d601395ada47e13712
Blankeos <carloantonioct@gmail.com>
2021-08-13 10:51:01 +0800

    Added middlewares for express-sessions, cookieParser, and passport. Also organized middlewares, routes, and databases

--- server/src/server.js
+++ server/src/server.js
@@ -2,32 +2,61 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import authRoutes from "./routes/authRoutes";
-import accountRoutes from './routes/accountRoutes'
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 dotenv.config();
 
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://localhost:3000", // This should be the domain where the client is  deployed on
+    credentials: true,
+  })
+);
 
-// app.use("/users", authRoutes);
-app.use(authRoutes)
+const secretCode = process.env.EXPRESS_SECRET || "secretcode";
+app.use(
+  session({
+    secret: secretCode,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser(secretCode));
+app.use(passport.initialize());
+app.use(passport.session());
+// -- Will be adding passport config here for the strategies --
+
+// Routes
+import authRoutes from "./routes/authRoutes";
+import accountRoutes from "./routes/accountRoutes";
+
+app.use(authRoutes);
 app.use(accountRoutes);
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chat-app', {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+// Database
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/chat-app",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
-mongoose.connection.on("connected", () => console.log("Database is connected!"));
+mongoose.connection.on("connected", () =>
+  console.log("Database is connected!")
+);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
