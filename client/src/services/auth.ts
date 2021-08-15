e17c64135752308d80d459e04496a06844529c40
Blankeos <carloantonioct@gmail.com>
2021-08-15 15:05:32 +0800

    Strictly typed the auth service. Much more expected return types

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -2,12 +2,16 @@ import axios from "axios";
 
 const authURL = "http://localhost:4000/auth";
 
+/**
+ * Creates a new user in the datbase.
+ * Returns a message from the backend regarding success of signUp.
+ */
 export const signUp = async (
   email: string,
   username: string,
   password: string
 ) => {
-  return axios({
+  const res = await axios({
     method: "POST",
     data: {
       email,
@@ -16,14 +20,25 @@ export const signUp = async (
     },
     withCredentials: true,
     url: `${authURL}/signup`,
-  }).then((res) => {
-    console.log(res);
-    return logIn(username, password);
   });
+
+  // console.log(res);
+
+  const message = res.data; // Was the sign up successful?
+  const { currentUser } = await logIn(username, password);
+
+  return {
+    message,
+    currentUser,
+  };
 };
 
+/**
+ * Logs the user in and creates a session of the browser in the database.
+ * Returns a message from the backend regarding success of logIn.
+ */
 export const logIn = async (usernameOrEmail: string, password: string) => {
-  return axios({
+  const res = await axios({
     method: "POST",
     data: {
       username: usernameOrEmail,
@@ -31,22 +46,40 @@ export const logIn = async (usernameOrEmail: string, password: string) => {
     },
     withCredentials: true,
     url: `${authURL}/login`,
-  }).then((res) => {
-    console.log(res);
-    return getCurrentUser();
   });
+
+  // console.log(res);
+
+  const message: string = res.data; // Was the log in successful?
+  const currentUser: UserType = await getCurrentUser();
+
+  return {
+    message,
+    currentUser,
+  };
 };
 
+/**
+ * Fetches the current user in session associated with this browser from the backend.
+ * Should return UserType if successful, otherwise an empty string.
+ * @returns UserType
+ */
 export const getCurrentUser = async () => {
-  return axios({
+  const res = await axios({
     method: "GET",
     withCredentials: true,
     url: `${authURL}/currentuser`,
-  }).then((res) => {
-    return res.data;
   });
+
+  // console.log(res);
+
+  const currentUser: UserType = res.data;
+  return currentUser;
 };
 
+/**
+ * Logs out the user and gets rid of its current session with the server.
+ */
 export const logOut = async () => {
   return axios({
     method: "GET",
@@ -54,3 +87,12 @@ export const logOut = async () => {
     url: `${authURL}/logout`,
   });
 };
+
+const auth = {
+  signUp,
+  logIn,
+  getCurrentUser,
+  logOut,
+};
+
+export default auth;
